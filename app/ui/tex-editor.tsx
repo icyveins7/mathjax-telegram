9c825eb13abe1335e38aa311d4773d3735618bb4
seomac <icyveins@hotmail.com>
2024-04-24 19:40:51 +0800

    remove the index.d.ts (wasn't needed) and fix on-the-fly mathjax
    now renders correctly by using both useState and useEffect to detect variable changes, set the value, and then trigger the re-render using mathjax.

--- app/ui/tex-editor.tsx
+++ app/ui/tex-editor.tsx
@@ -4,18 +4,16 @@ import {Button} from "@nextui-org/button";
 import {Textarea} from "@nextui-org/react";
 
 import {useState, useEffect} from "react";
-import MathJax from "./mathjax";
 
 export default function TexEditor() {
   let [tex, setTex] = useState("");
 
-  // useEffect(()=>{
-  //   if( typeof window?.MathJax !== "undefined"){
-  //     console.log("dynamic typeset useEffect");
-  //     window.MathJax.typeset();
-  //   }
-  // },[tex]);
-
+  useEffect(() => {
+    if(typeof window?.MathJax !== "undefined"){
+      window.MathJax.typeset();
+      console.log("useEffect typeset dynamic?");
+    }
+  }, [tex]);
 
   return (
     <div>
@@ -25,36 +23,13 @@ export default function TexEditor() {
           placeholder="Enter TeX here"
           className="max-w-xs"
           onValueChange={(value: string) => {
-            setTex(value);
+            setTex("$$" + value + "$$");
           }}
         />
       </div>
 
       <div>
-        <Button onClick={() => {
-          try{
-            window.MathJax.typeset();
-            console.log("typeset?");
-          } catch(e){
-            console.log(e);
-          }
-        }}>
-          Submit
-        </Button>
-      </div>
-
-      <div>
-        This is some static latex.
-        $$
-          \gamma = x^2 \tau
-        $$
-
-      </div>
-
-      <div>
-        $$
         {tex}
-        $$
       </div>
 
     </div>
