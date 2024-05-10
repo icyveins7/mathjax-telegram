fe1fbeda0159407b4e04ca663489d7fbc6e83ca8
seomac <icyveins@hotmail.com>
2024-05-10 11:28:46 +0800

    add ably-pubsub copied code, but think needs to be in serverless func

--- app/ui/tex-editor.tsx
+++ app/ui/tex-editor.tsx
@@ -5,61 +5,65 @@ import { Textarea } from "@nextui-org/react";
 
 import { useState, useEffect } from "react";
 
+import AblyPubSub from "./ably-pubsub";
+
 export default function TexEditor() {
-  let [tex, setTex] = useState("");
-  let [text, setText] = useState("");
+    let [tex, setTex] = useState("");
+    let [text, setText] = useState("");
 
-  useEffect(() => {
-    // check for .typeset directly, rather than just
-    // window.MathJax, otherwise it still seems to bug out
-    if (typeof window?.MathJax !== "undefined" && typeof window?.MathJax.typeset !== "undefined") {
-      // console.log("mathjax is present?");
-      window.MathJax.typeset();
-      // console.log("useEffect typeset dynamic?");
-    }
-  }, [tex]);
+    useEffect(() => {
+        // check for .typeset directly, rather than just
+        // window.MathJax, otherwise it still seems to bug out
+        if (typeof window?.MathJax !== "undefined" && typeof window?.MathJax.typeset !== "undefined") {
+            // console.log("mathjax is present?");
+            window.MathJax.typeset();
+            // console.log("useEffect typeset dynamic?");
+        }
+    }, [tex]);
 
-  return (
-    <div>
-      <div>
-        <Textarea
-          label="Your whiteboard"
-          placeholder="Enter TeX here"
-          className="max-w-xs"
-          value={text}
-          onValueChange={(value: string) => {
-            setText(value);
-            if (value.length > 0) {
-              setTex("$$" + value + "$$");
-            }
-            else {
-              setTex("");
-            }
-          }}
-        />
-      </div>
+    return (
+        <div>
+            <AblyPubSub />
 
-      <div>
-        {tex}
-      </div>
+            <div>
+                <Textarea
+                    label="Your whiteboard"
+                    placeholder="Enter TeX here"
+                    className="max-w-xs"
+                    value={text}
+                    onValueChange={(value: string) => {
+                        setText(value);
+                        if (value.length > 0) {
+                            setTex("$$" + value + "$$");
+                        }
+                        else {
+                            setTex("");
+                        }
+                    }}
+                />
+            </div>
 
-      <div>
-        <Button>
-          Submit
-        </Button>
-        <Button
-          onPress={() => {
-            setText("");
-            setTex("");
-          }}
-        >
-          Clear
-        </Button>
+            <div>
+                {tex}
+            </div>
+
+            <div>
+                <Button>
+                    Submit
+                </Button>
+                <Button
+                    onPress={() => {
+                        setText("");
+                        setTex("");
+                    }}
+                >
+                    Clear
+                </Button>
 
 
-      </div>
+            </div>
 
-    </div >
+        </div >
 
-  );
+    );
 }
