81ce0518688324c0bbfe2aa7cab8cc7b9a51040f
seomac <icyveins@hotmail.com>
2024-05-11 23:29:23 +0800

    use custom code instead of the imports from ably example
    some parts are working now: on connection function, channel getter and stuff
    but some others are not. can't display the messages?

--- app/ui/ably-pubsub.tsx
+++ app/ui/ably-pubsub.tsx
@@ -1,6 +1,6 @@
-'use client';
+// 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Ably from 'ably';
 import { AblyProvider, useChannel, useConnectionStateListener } from 'ably/react';
 
@@ -10,20 +10,53 @@ export default function AblyPubSub() {
 
     // Connect to Ably using the AblyProvider component and your API key
     console.log(process.env.ABLY_API);
+
+    // I don't know why this doesn't work with process.env.ABLY_API ??
     const client = new Ably.Realtime(
-        { key: process.env.ABLY_API }
+        process.env.ABLY_API
     );
     const [messages, setMessages] = useState<Ably.Message[]>([]);
 
-    useConnectionStateListener('connected', () => {
+    client.connection.on('connected', () => {
         console.log('Connected to Ably!');
     });
 
-    // Create a channel called 'get-started' and subscribe to all messages with the name 'first' using the useChannel hook
-    const { channel } = useChannel('get-started', 'first', (message) => {
-        console.log(message);
-        // setMessages(previousMessages => [...previousMessages, message]);
+    const channel = client.channels.get('get-started');
+    channel.subscribe((message) => {
+        // message.name;
+        // message.data;
+
+        // console.log(message.name);
+        // console.log(message.data);
+
+        setMessages(previousMessages => [...previousMessages, message]);
+        console.log(messages.length);
+        console.log(messages);
     });
+
+    let [messageRender, setMessageRender] = useState<React.JSX.Element[]>([]);
+    useEffect(() => {
+        setMessageRender(
+            messages.map(message => {
+                return <p key={message.id}>{message.data.body}</p>
+            })
+        );
+        // console.log("Updated messageRender");
+        // console.log(messages);
+        // console.log(messageRender);
+    }, [messages]);
+
+
+
+    // useConnectionStateListener('connected', () => {
+    //     console.log('Connected to Ably!');
+    // });
+    //
+    // Create a channel called 'get-started' and subscribe to all messages with the name 'first' using the useChannel hook
+    // const { channel } = useChannel('get-started', 'first', (message) => {
+    //     console.log(message);
+    //     // setMessages(previousMessages => [...previousMessages, message]);
+    // });
 
     return (
         <AblyProvider client={client}>
@@ -32,11 +65,12 @@ export default function AblyPubSub() {
                 <button onClick={() => { channel.publish('first', 'Here is my first message!') }}>
                     Publish
                 </button>
-                {
-                    messages.map(message => {
-                        return <p key={message.id}>{message.data.body}</p>
-                    })
-                }
+                <div>
+                    These are the messages:
+                    {
+                        messageRender
+                    }
+                </div>
             </div>
         </AblyProvider>
     );
